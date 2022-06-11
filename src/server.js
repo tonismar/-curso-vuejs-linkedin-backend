e218e9fdc43e68f928033f6ee26195202bcf7239
Tonismar Bernardo <tonismar@gmail.com>
2022-06-10 22:22:45 -0300

    Done

--- src/server.js
+++ src/server.js
@@ -2,18 +2,22 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { MongoClient } from 'mongodb'
 import path from 'path'
+import history from 'connect-history-api-fallback'
 
 const app = express()
 app.use(bodyParser.json())
-
 app.use('/images', express.static(path.join(__dirname, '../assets')))
+app.use(express.static(path.resolve(__dirname, '../dist'), { maxAge: '1y', etag: false }))
+app.use(history())
 
 app.get('/api/products', async (req, res) => {
     const client = await MongoClient.connect(
-        'mongodb://localhost:27017',
+        process.env.MONGO_USER && process.env.MONGO_PASS
+            ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DBNAME}.riflc.mongodb.net/?retryWrites=true&w=majority`
+            : 'mongodb://localhost:27017',
         { useNewUrlParser: true, useUnifiedTopology: true },
     )
-    const db = client.db('linkding-vue-course')
+    const db = client.db(process.env.MONGO_DBNAME || 'linkding-vue-course')
     const products = await db.collection('products').find({}).toArray()
     res.status(200).json(products)
     client.close()
@@ -22,10 +26,12 @@ app.get('/api/products', async (req, res) => {
 app.get('/api/users/:userId/cart', async (req, res) => {
     const { userId } = req.params;
     const client = await MongoClient.connect(
-        'mongodb://localhost:27017',
+        process.env.MONGO_USER && process.env.MONGO_PASS
+            ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DBNAME}.riflc.mongodb.net/?retryWrites=true&w=majority`
+            : 'mongodb://localhost:27017',
         { useNewUrlParser: true, useUnifiedTopology: true },
     )
-    const db = client.db('linkding-vue-course')
+    const db = client.db(process.env.MONGO_DBNAME || 'linkding-vue-course')
     const user = await db.collection('users').findOne({ id: userId })
     if (!user) return res.status(404).json('Could not find user')
     const products = await db.collection('products').find({}).toArray()
@@ -39,10 +45,12 @@ app.get('/api/users/:userId/cart', async (req, res) => {
 app.get('/api/products/:productId', async (req, res) => {
     const { productId } = req.params
     const client = await MongoClient.connect(
-        'mongodb://localhost:27017',
+        process.env.MONGO_USER && process.env.MONGO_PASS
+            ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DBNAME}.riflc.mongodb.net/?retryWrites=true&w=majority`
+            : 'mongodb://localhost:27017',
         { useNewUrlParser: true, useUnifiedTopology: true },
     )
-    const db = client.db('linkding-vue-course')
+    const db = client.db(process.env.MONGO_DBNAME || 'linkding-vue-course')
     const product = await db.collection('products').findOne({ id: productId })
 
     if (product) {
@@ -57,10 +65,12 @@ app.post('/api/users/:userId/cart', async (req, res) => {
     const { userId } = req.params
     const { productId } = req.body
     const client = await MongoClient.connect(
-        'mongodb://localhost:27017',
+        process.env.MONGO_USER && process.env.MONGO_PASS
+            ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DBNAME}.riflc.mongodb.net/?retryWrites=true&w=majority`
+            : 'mongodb://localhost:27017',
         { useNewUrlParser: true, useUnifiedTopology: true },
     )
-    const db = client.db('linkding-vue-course')
+    const db = client.db(process.env.MONGO_DBNAME || 'linkding-vue-course')
     await db.collection('users').updateOne({ id: userId }, {
         $addToSet: { cartItems: productId },
     })
@@ -76,10 +86,12 @@ app.post('/api/users/:userId/cart', async (req, res) => {
 app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
     const { userId, productId } = req.params
     const client = await MongoClient.connect(
-        'mongodb://localhost:27017',
+        process.env.MONGO_USER && process.env.MONGO_PASS
+            ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DBNAME}.riflc.mongodb.net/?retryWrites=true&w=majority`
+            : 'mongodb://localhost:27017',
         { useNewUrlParser: true, useUnifiedTopology: true },
     )
-    const db = client.db('linkding-vue-course')
+    const db = client.db(process.env.MONGO_DBNAME || 'linkding-vue-course')
     await db.collection('users').updateOne({ id: userId }, {
         $pull: { cartItems: productId },
     })
@@ -92,6 +104,10 @@ app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
     client.close()
 })
 
-app.listen(9000, () => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+
+app.listen(process.env.PORT || 9000, () => {
     console.log('Server is listening on port 9000!')
 })
